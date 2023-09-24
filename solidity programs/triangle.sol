3f34b802882030c9330d99d99eb2e0c15cdde6bf
codewithamans <a.jagat159753@gmail.com>
2023-09-24 21:24:28 +0530

    fixes

---
+++ solidity programs/triangle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract RightAngledTriangle {
+    //To check if a triangle with side lenghts a,b,c is a right angled triangle
+   function max(uint x, uint y) private pure returns (uint) {
+        return x > y ? x : y;
+    }
+function check(uint a, uint b, uint c) public pure returns (bool) {
+uint longest = max(max(a, b), c);
+       if (longest==0 || longest==1){
+    return false;
+}
+        
+   else if(longest == c) {
+            return a*a + b*b == c*c;
+        } else if(longest == b) {
+            return a*a + c*c == b*b;
+        } else {
+            return b*b + c*c == a*a;
+        }
+        
+    }
+}
